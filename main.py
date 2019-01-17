89af576049261060e9c1476309c2acf0a9e414e7
jon mallozzi <blaze@localhost.localdomain>
2019-01-17 14:49:19 -0500

    first commit with my app only out putting hello and the date compiled

---
+++ main.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+#prints the date the program was ran
+print()
+print (datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+print ("hi and welcome to my dumb python app")
